f31de8c51dcae6ceedea85db08acd15e4847f151
guts-718 <rahul718rg@gmail.com>
2024-03-14 22:11:38 +0530

    search page and filter added

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -5,6 +5,7 @@ import AuthCallbackPage from './pages/AuthCallbackPage';
 import UserProfilePage from './pages/UserProfilePage';
 import ProtectedRoute from './auth/ProtectedRoute';
 import ManageRestaurantPage from './pages/ManageRestaurantPage';
+import SearchPage from './pages/SearchPage';
 //import ManageRestaurantPage from './forms/manage-restaurant-form/ManageRestaurantPage';
 
 const AppRoutes=()=>{
@@ -13,6 +14,7 @@ const AppRoutes=()=>{
 
             <Route path="/" element={<Layout showHero><HomePage/></Layout>}/>
             <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
+            <Route path="/search/:city" element={<Layout showHero={false} ><SearchPage/></Layout>}/>
             
             <Route element={<ProtectedRoute/>}>
                  <Route path="/user-profile" element={<Layout><UserProfilePage/></Layout>}/>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,26 @@
 import landingImage from "../assets/landing.png"
 import appDownloadImage from "../assets/appDownload.png"
+import SearchBar, {SearchForm} from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 const HomePage = () => {
+    const navigate=useNavigate();
+  const handleSearchSubmit=(searchFormValues: SearchForm)=>{
+   
+    navigate({
+        pathname: `/search/${searchFormValues.searchQuery}`,
+    })
+
+
+  }
   return(
     <div className="flex flex-col gap-12">
         {/* text block -- flex-col  hai islie hamesha h1 and span different column wise hi appear karega .. gap-5{children ke bich y ke along gap} mx-16 mtlb window aur div ke bich x ke along space*/}
-        <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16 mx-16">
+        <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16 mx-16">
             <h1 className="text-5xl font-bold tracking-tight text-orange-600">
                 Tuck into a takeaway today
             </h1>
             <span className="text-xl">Food is just a click away!</span>
+            <SearchBar placeHolder="Search by city or town" onSubmit={handleSearchSubmit}/>
         </div>
 
 
